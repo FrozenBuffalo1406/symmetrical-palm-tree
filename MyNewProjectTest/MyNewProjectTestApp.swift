db48cbb7fd441c8027e799586b7094fdf21fa606
FrozenBuffalo1406 <ahmadfauzirizky003@gmail.com>
2026-06-04 09:26:50 +0700

    Initial Commit

---
+++ MyNewProjectTest/MyNewProjectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewProjectTestApp.swift
+//  MyNewProjectTest
+//
+//  Created by Ahmad Fauzi Rizky on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MyNewProjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
